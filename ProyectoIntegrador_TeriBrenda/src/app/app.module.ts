44e7aef4b2920f89b3d059cbb27073c8fa0dd96e
Brenda Teri <brendateri@gmail.com>
2022-05-24 22:52:41 -0300

    Creación de header, banner, logo del programa y redes sociales

--- ProyectoIntegrador_TeriBrenda/src/app/app.module.ts
+++ ProyectoIntegrador_TeriBrenda/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPrComponent } from './components/logo-arg-pr/logo-arg-pr.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPrComponent,
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoIntegrador_TeriBrenda/src/app/components/logo-arg-pr/logo-arg-pr.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoArgPrComponent } from './logo-arg-pr.component';
+
+describe('LogoArgPrComponent', () => {
+  let component: LogoArgPrComponent;
+  let fixture: ComponentFixture<LogoArgPrComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoArgPrComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoArgPrComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
